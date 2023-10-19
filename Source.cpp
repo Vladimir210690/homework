13d85d33535511f13900f2ae641daf5c8009b243
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-10-19 13:22:01 +0300

    Анкета
    Задача анкеты собрать данные о пользователе и погрузить их в массив с последующим извлечением.

--- Source.cpp
+++ Source.cpp
@@ -3,27 +3,49 @@
 using namespace std;
 
 int main()
-
 {
 	setlocale(LC_ALL, "RUS");
+	const int DATA = 10;
+	int age[DATA];
+	char name[DATA];
 
-	const int SAW = 10;
-	int keis[SAW];
-	int j = 0;
+	cout << "Введите ваше имя: " << endl;
+	cin >> name;
 
-	for (int h = 0; h < SAW; h++)
+	for (int c = 0; c < DATA; c++)
 	{
-		cin >> j;
-		keis[h] = j;
+		switch (c)
+		{
+		case 0:
+			cout << "Сколько вам лет? " << endl;
+			cin >> age[c];
+			break;
+		case 1:
+			cout << "Какой ваш рост в сантиметрах? " << endl;
+			cin >> age[c];
+			break;
+		case 2:
+			cout << "Какой ваш вес в килограммах? " << endl;
+			cin >> age[c];
+			break;
+		case 3:
+			cout << "Какой ваш заработок в рублях? " << endl;
+			cin >> age[c];
+			break;
+		case 4:
+			cout << "Сколько лет работаете в вашей профессии? " << endl;
+			cin >> age[c];
+			break;
+		}
 	}
+	cout << "Анкетные данные собраны: " << endl << endl;
 
-	cout << "РњР°СЃСЃРёРІ Р·Р°РіСЂСѓР¶РµРЅ! " << endl << endl;
+	cout << "Имя исследуемого: " << name << endl;
+	cout << "Возраст исследуемого: " << age[0] << " лет" << endl;
+	cout << "Рост исследуемого: " << age[1] << " см" << endl;
+	cout << "Вес исследуемого: " << age[2] << " кг" << endl;
+	cout << "Заработок исследуемого: " << age[3] << " руб" << endl;
+	cout << "Стаж работы исследуемого: " << age[4] << " лет" << endl;
 
-	for (int h = 0; h < SAW; h++)
-	{
-		cout << keis[h] << endl;
-	}
-
-	
 	return 0;
 }
