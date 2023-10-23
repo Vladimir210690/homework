66c95e5f9f5d07b062b2af7d17216043168cae80
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-10-23 16:37:10 +0300

    Змейка, урок 2
    На этом уроке я запрограммировал клавиши для движения змейки, и написал логику движения змейки и поедания фруктов

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,58 @@
 #include <iostream>
+#include <ctime>
+#include <conio.h>
 
 using namespace std;
 
-const int ROW = 11;
-const int COL = 13;
+const int ROW = 20;
+const int COL = 40;
+
+int x = 0;
+int y = 0;
+int fruX = 0;
+int fruY = 0;
+bool gameOver = false;
+enum motion {STOP = 0, LEFT, RIGHT, UP, DOWN};
+motion run;
+
+int score = 0;
+
+void Setup()
+{
+	srand(time(NULL));
+
+	x = ROW / 2 - 1;
+	y = COL / 2 - 1;
+
+	fruX = rand() % ROW + 1;
+	fruY = rand() % COL + 1;
+}
 
 void Draw()
 {
 	system("cls");
 
-	for (int a = 0; a < ROW; a++)
+	for (int a = 0; a < COL; a++)
 	{
-		for (int b = 0; b < COL; b++)
+		cout << "#";
+	}
+	cout << endl;
+
+	for (int b = 0; b < ROW; b++)
+	{
+		for (int c = 0; c < COL; c++)
 		{
-			if (b == 4 || b == 9)
+			if (c == 0 || c == 39)
 			{
 				cout << "#";
 			}
-			if (a == 3 || a == 7)
+			else if (b == x  && c == y)
 			{
-				cout << "#";
+				cout << "0";
+			}
+			else if (b == fruX && c == fruY)
+			{
+				cout << "*";
 			}
 			else
 			{
@@ -29,14 +62,82 @@ void Draw()
 		cout << endl;
 	}
 
+	for (int a = 0; a < COL; a++)
+	{
+		cout << "#";
+	}
+	cout << endl;
+}
+
+void Input()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+		case 'a':
+			run = LEFT;
+			break;
+		case 'w':
+			run = UP;
+			break;
+		case 'd':
+			run = RIGHT;
+			break;
+		case 's':
+			run = DOWN;
+			break;
+		case 'p':
+			gameOver = true;
+			break;
+		}
+	}
+}
+
+void Logik()
+{
+	switch (run)
+	{
+	case LEFT:
+		y--;
+		break;
+	case RIGHT:
+		y++;
+		break;
+	case UP:
+		x--;
+		break;
+	case DOWN:
+		x++;
+		break;
+	}
+
+	if ((x > ROW || x < 0) || (y > COL || y < 0))
+	{
+		gameOver = true;
+	}
+
+	if (x == fruX && y == fruY)
+	{
+		score++;
+		fruX = rand() % ROW + 1;
+		fruY = rand() % COL + 1;
+	}
+	cout << "You game score: " << score << endl;
 }
 
 int main()
+
 {
-	while (true)
+	Setup();
+
+	while (gameOver == false)
 	{
 		Draw();
+		Input();
+		Logik();
 	}
 	
+	cout << "Game Ower" << endl;
 	return 0;
 }
