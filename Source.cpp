a060ec365a9b3d449f9fc9456d81983a3e337d1d
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-10-23 10:59:33 +0300

    Экран для крестиков-ноликов
    Сам отрисовал в консоле экран для игры крестики-нолики

--- Source.cpp
+++ Source.cpp
@@ -2,24 +2,22 @@
 
 using namespace std;
 
-const int ROW = 20;
-const int COL = 40;
+const int ROW = 11;
+const int COL = 13;
 
 void Draw()
 {
 	system("cls");
 
-	for (int a = 0; a < COL; a++)
+	for (int a = 0; a < ROW; a++)
 	{
-		cout << "#";
-	}
-	cout << endl;
-
-	for (int b = 0; b < ROW; b++)
-	{
-		for (int c = 0; c < COL; c++)
+		for (int b = 0; b < COL; b++)
 		{
-			if (c == 0 || c == 39)
+			if (b == 4 || b == 9)
+			{
+				cout << "#";
+			}
+			if (a == 3 || a == 7)
 			{
 				cout << "#";
 			}
@@ -31,20 +29,14 @@ void Draw()
 		cout << endl;
 	}
 
-	for (int a = 0; a < COL; a++)
-	{
-		cout << "#";
-	}
-	cout << endl;
 }
 
 int main()
 {
-	setlocale(LC_ALL, "RU");
-
 	while (true)
 	{
 		Draw();
 	}
-
+	
+	return 0;
 }
