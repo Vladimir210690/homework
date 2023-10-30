d4433d34e4b0d24d0e9a14f5494fd77da768da65
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-10-30 19:05:30 +0300

    Рамочка для пользователя
    Работа в классе, пользователь вводит длину и ширину - ему выводится рамочка

--- Source.cpp
+++ Source.cpp
@@ -1,157 +1,47 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
-const int ROW = 20;
-const int COL = 40;
-
-int x = COL / 2;
-int y = ROW / 2;
-
-
-int gunX = COL / 2;
-int gunY = ROW / 2;
-
-
-int gunX2 = COL / 2;
-int gunY2 = ROW / 2;
-
-bool gameOver = false;
-int enemyX = rand() % COL;
-int enemyY = ROW - 20;
-
-int score = 0;
-bool gameOwer = false;
-enum qwe { STOP = 0, FIRE , LEFT, RIGHT, UP, DOWN };
-qwe run;
-
-void Draw() // –§—É–Ω–∫—Ü–∏—è –æ—Ç—Ä–∏—Å–æ–≤–∫–∏ –∏–≥—Ä–æ–≤–æ–≥–æ –ø–æ–ª—è
-{
-    system("cls");
-
-    for (int a = 0; a < COL; a++)
-    {
-        cout << "#";
-    }
-    cout << endl;
-
-    for (int b = 0; b < ROW; b++)
-    {
-        for (int c = 0; c < COL; c++)
-        {
-            if (c == 0 || c == 39)
-            {
-                cout << "#";
-            }
-            else if ((c == x - 1 || c == x + 1 || c == x) && (b == y + 9))
-            {
-                cout << "#";
-            }
-     
-            else if (c == gunX && b == gunY + 8)
-            {
-                cout << "^";
-            }
-            else if (c == gunX2 && b == gunY2 + 8)
-            {
-                cout << "^";
-            }
-            else if (c == enemyX && b == enemyY)
-            {
-                cout << "*";
-            }
-            else
-            {
-                cout << " ";
-            }
-        }
-        cout << endl;
-    }
-
-    for (int a = 0; a < COL; a++)
-    {
-        cout << "#";
-    }
-    cout << endl;
-}
-
-
-void Input() // –§—É–Ω–∫—Ü–∏—è –æ—Ç—Å–ª–µ–∂–∏–≤–∞–Ω–∏—è –≤–≤–æ–¥–∞ —Å –∫–ª–∞–≤–∏–∞—Ç—É—Ä—ã
-{
-    if (_kbhit())
-    {
-        switch (_getch())
-        {
-        case 'a':
-            run = LEFT;
-            break;
-        case 'd':
-            run = RIGHT;
-            break;
-        case 'w':
-            run = UP;
-            break;
-        case 's':
-            run = DOWN;
-            break;
-
-        }
-    }
-}
-
-void Logik() // –§—É–Ω–∫—Ü–∏—è –ª–æ–≥–∏–∫–∏ 
-{
-    enemyY++;
-    gunY--;
-
-    switch (run)
-    {
-    case LEFT:
-        x--;
-        gunX--;
-        gunX2--;
-        break;
-    case RIGHT:
-        x++;
-        gunX++;
-        gunX2++;
-        break;
-    }
-
-    if (gunY == - 10)
-    {
-        gunY = 10;
-    }
-    if (enemyY == 21)
-    {
-        enemyX = rand() % COL;
-        enemyY = ROW - 20;
-    }
-    else if ((enemyY == gunY && enemyX == gunX) || (enemyY == gunY2 && enemyX == gunX2))
-    {
-        score = score + 5;
-        enemyX = rand() % COL;
-        enemyY = ROW - 20;
-    }
-    else if (enemyY == y + 9 && (enemyX == x || enemyX == x - 1 || enemyX == x + 1))
-    {
-        score++;
-        enemyX = rand() % COL;
-        enemyY = ROW - 20;
-    }
-   
-
-    cout << "Score: " << score << endl;
-}
-
 int main()
 {
-    while (!gameOwer)
-    {
-       Draw();
-       Input();
-       Logik();
-    }
-    return 0;
+	setlocale(LC_ALL, "RU");
+
+	int row = 0;
+	int col = 0;
+
+	cout << "¬‚Â‰ËÚÂ ÍÓÎË˜ÂÒÚ‚Ó ÒÚÓ˜ÂÍ" << endl;
+	cin >> row;
+
+	cout << "¬‚Â‰ËÚÂ ÍÓÎË˜ÂÒÚ‚Ó ÒÚÓÎ·ˆÓ‚" << endl;
+	cin >> col;
+
+	for (int a = 0; a < col; a++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+
+	for (int b = 0; b < row; b++)
+	{
+		for (int c = 0; c < col; c++)
+		{
+			if (c == 0 || c == col - 1)
+			{
+				cout << "*";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+
+	for (int a = 0; a < col; a++)
+	{
+		cout << "*";
+	}
+	cout << endl;
+
+	return 0;
 }
