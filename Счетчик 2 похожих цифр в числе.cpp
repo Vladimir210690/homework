97f623541b0bfe3445a5b21976c2737e4671fac0
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2023-11-14 19:16:46 +0300

    Счетчик 1
    Задание подсчитать количество чисел в диапазоне от 100 до 900 в составе которых 2 цифры равны друг другу.

---
+++ Счетчик 2 похожих цифр в числе.cpp
@@ -0,0 +1,33 @@
+//Подсчитать количество целых чисел в диапазоне от 100 до 900,
+//у которых в числе есть 2 одинаковые цифры
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int col = 0; //Счетчик количества чисел
+
+	int one = 0; //Разряды числа
+	int two = 0;
+	int three = 0;
+
+	int two1 = 0;
+	int three1 = 0; //Остатки от операции
+	
+	for (int i = 100; i < 901; i++) {
+
+		three = i % 10;
+		three1 = i / 10;
+		two = three1 % 10;
+		two1 = three1 / 10;
+		one = two1 % 10;
+		
+		if (three == two || three == one) {
+			cout << i << endl;
+			col++;
+		}
+	}
+	cout << "Quantity: " << col;
+	return 0;
+}
