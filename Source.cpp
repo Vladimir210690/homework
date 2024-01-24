9addd85107b02683426b3f2900343a6eb9f31610
Vladimir <146058049+Vladimir210690@users.noreply.github.com>
2024-01-24 20:03:02 +0300

    Push_front

--- Source.cpp
+++ Source.cpp
@@ -1,61 +1,55 @@
-//Написать программу, реализующую сортировку
-//массива с помощью усовершенствованной
-//сортировки пузырьковым методом.
-//Усовершенствование состоит в том, чтобы
-//анализировать количество перестановок на каждом
-//шагу, если это количество равно нулю, то продолжать
-//сортировку нет смысла — массив отсортирован.
-
-
 #include <iostream>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
+struct NODE { // Создаем ячейку списка
+	int number;
+	string name;
+	NODE* next;
+};
+
+struct List { // Сoздаем голову списка
+	int col;
+	NODE* first;
+};
+
+void push_beak(List* lst, int col) { // Функция добавления новой структуры в список
+	string go;
+	NODE* node = new NODE;
+	node->number = col;
+	cin >> go;
+	node->name = go;
+	node->next = lst->first;
+	lst->first = node;
+	lst->col++;
+}
+
+void print(List* lst) { // Функция вывода листа
+	NODE* node = lst->first;
+	while (node != nullptr) {
+		cout << node->number << "\t" << node->name << endl;
+		node = node->next;
+	}
+
+}
+
 int main() {
-	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
 
-	const int SIZE = 10;
-	int bubble[SIZE]{};
+	int run = 5;
+	List head;
+	head.col = 0;
+	head.first = nullptr;
 
-	bool flag = false;
-	int col = 0;
-	int iter = 0;
-
-	for (int i = 0; i < SIZE; i++) {
-		bubble[i] = rand() % 100;
-		cout << bubble[i] << " ";
+	for (int i = 0; i < run; i++) {
+		push_beak(&head, rand() % 100);
+		
 	}
-	cout << endl << endl;
 
-	for (int i = 0; i < SIZE; i++) {
-		for (int j = 0; j < SIZE - 1; j++) {
-			if (bubble[j] > bubble[j + 1]) {
-				swap(bubble[j], bubble[j + 1]);
-			}
-			else if (bubble[j] < bubble[j + 1]) {
-				col += 1;
-			}
-			iter += 1;
-			cout << col << " ";
-			if (col == 9) {
-				flag = true;
-				break;
-			}
-		}
-		col = 0;
-		if (flag == true) {
-			break;
-		}
+	print(&head);
 	
-	}
-	cout << "Количество пробегов = " << iter << endl << endl;
-
-	for (int i = 0; i < SIZE; i++) {
-		cout << bubble[i] << " ";
-	}
-
 
 	return 0;
 }
